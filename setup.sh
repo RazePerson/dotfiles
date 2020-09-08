99ecd829ed24751e8380a62437a0cacf9f75cc7e
Tamas Razman <Tamas.Razman@ngi.systems>
2020-09-08 20:23:04 +0300

    Fixed script

--- setup.sh
+++ setup.sh
@@ -4,11 +4,10 @@ set -euxo pipefail
 
 install_before_reboot() {
 
-	echo "Updating and installing vim, the tools for it and git"
+	echo "Updating and installing vim, the tools for it"
 	apt update -y
 	apt install zsh -y
   apt install powerline fonts-powerline -y
-	apt install git -y
 
 	echo "Cloning Oh My Zsh repo"
 	git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
@@ -56,7 +55,7 @@ if [ ! -f /var/run/resume-after-reboot ]; then
 
   # add this script to zsh so it gets triggered immediately after reboot
   # change it to .bashrc if using bash shell
-  echo "$script" >> ~/.zshrc
+  echo "$script" >> ~/.bashrc
 
   # create a flag file to check if we are resuming from reboot.
   touch /var/run/resume-after-reboot
@@ -67,9 +66,10 @@ if [ ! -f /var/run/resume-after-reboot ]; then
 
 else
   echo "resuming script after reboot.."
+	sleep 3
 
   # Remove the line that we added in zshrc
-  sed -i '/bash/d' ~/.zshrc
+  sed -i '/bash/d' ~/.bashrc
 
   # remove the temporary file that we created to check for reboot
   sudo rm -f /var/run/resume-after-reboot
